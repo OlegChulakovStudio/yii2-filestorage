530fafa2b0a743469de94a9c4bb48ce71e855950
Vladmir <tzendos@mail.ru>
2017-12-11 18:42:51 +0300

    Тип исключения для ошибочной загрузки файла

---
+++ src/exceptions/NotUploadFileException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Файл класса NotUploadFileException.php
+ *
+ * @copyright Copyright (c) 2017, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+namespace chulakov\filestorage\exceptions;
+
+/**
+ * Не удалось загрузить файл
+ *
+ * Class NotUploadFileException
+ * @package chulakov\components\exceptions
+ */
+class NotUploadFileException extends \Exception
+{
+
+}
