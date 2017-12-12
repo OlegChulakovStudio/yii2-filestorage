81330707dad768bff3c54473f174de6fe82f899a
Ilya Karavaev <ik@chulakov.ru>
2017-12-12 18:18:56 +0300

    Изменение интерфейса сервиса обработки файлов

--- src/models/services/FileService.php
+++ src/models/services/FileService.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Файл класса FileService.php
+ * Файл класса FileService
  *
  * @copyright Copyright (c) 2017, Oleg Chulakov Studio
  * @link http://chulakov.com/
@@ -9,14 +9,19 @@
 namespace chulakov\filestorage\models\services;
 
 use chulakov\filestorage\models\Image;
-use chulakov\filestorage\models\repositories\FileRepository;
 use chulakov\filestorage\models\File;
-use yii\web\UploadedFile;
+use chulakov\filestorage\models\BaseFile;
+use chulakov\filestorage\models\repositories\FileRepository;
+use chulakov\filestorage\uploaders\UploadInterface;
+use chulakov\filestorage\UploadParams;
+use yii\base\UnknownClassException;
 
 class FileService
 {
-    /** @var FileRepository */
-    private $repository;
+    /**
+     * @var FileRepository
+     */
+    protected $repository;
 
     public function __construct(FileRepository $repository)
     {
@@ -24,27 +29,50 @@ class FileService
     }
 
     /**
-     * @param UploadedFile $uploadedFile
-     * @param array $config
+     * @param UploadInterface $uploadedFile
+     * @param UploadParams $params
      * @return File|null
-     * @throws \Exception
+     * @throws UnknownClassException
      */
-    public function createFile(UploadedFile $uploadedFile, array $config = [])
+    public function createFile(UploadInterface $uploadedFile, UploadParams $params)
     {
-        $file = new File($uploadedFile, $config);
-        return $this->repository->save($file) ? $file : null;
+        return $this->createUpload(File::class, $uploadedFile, $params);
     }
 
     /**
-     * @param UploadedFile $uploadedFile
-     * @param array $config
+     * @param UploadInterface $uploadedFile
+     * @param UploadParams $params
      * @return Image|null
-     * @throws \Exception
+     * @throws UnknownClassException
      */
-    public function createImage(UploadedFile $uploadedFile, array $config = [])
+    public function createImage(UploadInterface $uploadedFile, UploadParams $params)
     {
-        $file = new Image($uploadedFile, $config);
-        return $this->repository->save($file) ? $file : null;
+        return $this->createUpload(Image::class, $uploadedFile, $params);
+    }
+
+    /**
+     * @param $class
+     * @param UploadInterface $file
+     * @param UploadParams $params
+     * @return mixed
+     * @throws UnknownClassException
+     */
+    protected function createUpload($class, UploadInterface $file, UploadParams $params)
+    {
+        if (!class_exists($class)) {
+            throw new UnknownClassException('Класс для создания файла не найден.');
+        }
+        /** @var BaseFile $model */
+        $model = new $class();
+
+        $model->group_code = $params->group_code;
+        $model->object_id = $params->object_id;
+        $model->ori_extension = $file->getExtension();
+        $model->ori_name = $file->getBaseName();
+        $model->mime = $file->getType();
+        $model->size = $file->getSize();
+
+        return $model;
     }
 
     public function update()
