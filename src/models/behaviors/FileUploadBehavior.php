22052676dfb6ee7ac49a723eebcf85d2f63ca937
Ilya Karavaev <ik@chulakov.ru>
2017-12-12 18:18:21 +0300

    Изменение интерфейса поведений загрузки файлов

--- src/models/behaviors/FileUploadBehavior.php
+++ src/models/behaviors/FileUploadBehavior.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Файл класса FileUploadBehavior.php
+ * Файл класса FileUploadBehavior
  *
  * @copyright Copyright (c) 2017, Oleg Chulakov Studio
  * @link http://chulakov.com/
@@ -8,34 +8,119 @@
 
 namespace chulakov\filestorage\models\behaviors;
 
+use yii\di\Instance;
+use yii\base\Model;
 use yii\base\Behavior;
-use yii\db\ActiveRecord;
-use yii\web\UploadedFile;
+use chulakov\filestorage\FileStorage;
+use chulakov\filestorage\UploadParams;
+use chulakov\filestorage\uploaders\UploadInterface;
+use chulakov\filestorage\exceptions\NotUploadFileException;
 
 class FileUploadBehavior extends Behavior
 {
+    /**
+     * @var Model
+     */
+    public $owner;
+    /**
+     * @var string
+     */
     public $attribute;
+    /**
+     * @var string
+     */
+    public $group = 'default';
+    /**
+     * @var bool
+     */
+    public $save = false;
+    /**
+     * @var string|UploadInterface
+     */
+    public $repository;
+    /**
+     * @var string|FileStorage
+     */
+    public $storage;
 
+    /**
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        $this->storage = Instance::ensure($this->storage);
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function events()
     {
         return [
-            ActiveRecord::EVENT_BEFORE_VALIDATE => 'beforeValidate',
+            Model::EVENT_BEFORE_VALIDATE => 'beforeValidate',
         ];
     }
 
+    /**
+     * Проверка и инициализация данных перед валидацией модели
+     */
     public function beforeValidate()
     {
-        if ($this->owner->{$this->attribute} instanceof UploadedFile) {
+        if ($this->isInstances($this->owner->{$this->attribute})) {
             return;
         }
+        $files = $this->getInstances();
+        if ($this->isInstances($files)) {
+            $this->owner->{$this->attribute} = $files;
+        }
+    }
 
-        $file = UploadedFile::getInstance($this->owner, $this->attribute);
+    /**
+     * Проверка, объявлены ли уже данные в модели
+     *
+     * @param mixed $model
+     * @return bool
+     */
+    protected function isInstances($model)
+    {
+        return $model instanceof $this->repository;
+    }
 
+    /**
+     * Инициализация данных для модели
+     *
+     * @return mixed
+     */
+    protected function getInstances()
+    {
+        $repository = $this->repository;
+        $file = $repository::getInstance($this->owner, $this->attribute);
         if (empty($file)) {
-            $file = UploadedFile::getInstanceByName($this->attribute);
+            $file = $repository::getInstanceByName($this->attribute);
         }
-        if ($file instanceof UploadedFile) {
-            $this->owner->{$this->attribute} = $file;
+        return $file;
+    }
+
+    /**
+     * Загрузка и сохранение файлов
+     *
+     * @return mixed
+     * @throws NotUploadFileException
+     * @throws \Exception
+     */
+    public function upload()
+    {
+        /** @var UploadInterface $files */
+        $files = $this->owner->{$this->attribute};
+        if (!$this->isInstances($files)) {
+            throw new NotUploadFileException('Нет файлов для сохранения.');
         }
+        $params = new UploadParams($this->group);
+        $params->save = $this->save;
+        if (method_exists($this->owner, 'getId')) {
+            $params = $this->owner->getId();
+        }
+        return $this->storage->uploadFile($files, $params);
     }
 }
