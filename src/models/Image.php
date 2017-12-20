ead6353ad39bfc60214311d5dc597410ff9b0743
Vladimir <tzendos@mail.ru>
2017-12-20 19:03:55 +0300

    Подключил ThumbBehavior

--- src/models/Image.php
+++ src/models/Image.php
@@ -8,7 +8,25 @@
 
 namespace chulakov\filestorage\models;
 
+use chulakov\filestorage\behaviors\ThumbBehavior;
+use chulakov\filestorage\params\ThumbParams;
+use sem\helpers\ArrayHelper;
+
+/**
+ * Class Image
+ * @package chulakov\filestorage\models
+ *
+ * @method string thumb(ThumbParams $thumbParams)
+ */
 class Image extends BaseFile
 {
-    protected $imageManager;
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(),
+            [
+                [
+                    'class' => ThumbBehavior::className()
+                ]
+            ]);
+    }
 }
