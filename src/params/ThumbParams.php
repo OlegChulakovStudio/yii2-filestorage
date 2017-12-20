c1053c41279b1e1f5934c8c8e88d3bed4bfdd51e
Vladimir <tzendos@mail.ru>
2017-12-20 19:01:46 +0300

    Добавил структуру настройки для thumbnail

---
+++ src/params/ThumbParams.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Файл класса ThumbParams
+ *
+ * @copyright Copyright (c) 2017, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+namespace chulakov\filestorage\params;
+
+class ThumbParams
+{
+    /**
+     *  Ширина
+     *
+     * @var integer
+     */
+    public $width;
+    /**
+     * Высота
+     *
+     * @var integer
+     */
+    public $height;
+    /**
+     * Расширение
+     *
+     * @var string
+     */
+    public $extension;
+    /**
+     * Качество
+     *
+     * @var int
+     */
+    public $quality = 100;
+    /**
+     * Путь к файлу с watermark
+     *
+     * @var string
+     */
+    public $watermarkPath;
+    /**
+     * Позиция watermark
+     *
+     * @var integer
+     */
+    public $watermarkPosition;
+    /**
+     * Куда сохранить изображение
+     *
+     * @var string
+     */
+    public $savedPath;
+
+    /**
+     * ThumbParams constructor.
+     * @param integer $width
+     * @param integer $height
+     */
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+}
