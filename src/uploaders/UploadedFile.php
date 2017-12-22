a922b83ad99054ed3bf1501a941a58ae599d0bde
Vladimir <tzendos@mail.ru>
2017-12-22 17:54:54 +0300

    Загрузка обычных файлов

--- src/uploaders/UploadedFile.php
+++ src/uploaders/UploadedFile.php
@@ -8,8 +8,53 @@
 
 namespace chulakov\filestorage\uploaders;
 
+use chulakov\filestorage\savers\SaveInterface;
+
+/**
+ * Class UploadedFile
+ * @package chulakov\filestorage\uploaders
+ */
 class UploadedFile extends \yii\web\UploadedFile implements UploadInterface
 {
+    /**
+     * Менеджер сохранения
+     *
+     * @var SaveInterface
+     */
+    public $saveManager;
+
+    /**
+     * @inheritdoc
+     */
+    public function saveAs($file, $deleteTempFile = true)
+    {
+        $this->saveManager->save($file, $this->tempName, $deleteTempFile);
+    }
+
+    /**
+     * Проверка, выполнялось ли сохранение или нет
+     *
+     * @return bool
+     */
+    protected function isSaved()
+    {
+        return $this->saveManager && $this->saveManager->isSaved();
+    }
+
+    /**
+     * Получить расширение файла
+     *
+     * @return string
+     */
+    public function getExtension()
+    {
+        $ext = $this->saveManager->getExtension();
+        if (empty($ext)) {
+            return strtolower(pathinfo($this->name, PATHINFO_EXTENSION));
+        }
+        return $ext;
+    }
+
     /**
      * Получение MIME типа файла
      *
@@ -17,7 +62,7 @@ class UploadedFile extends \yii\web\UploadedFile implements UploadInterface
      */
     public function getType()
     {
-        return $this->type;
+        return $this->saveManager->getType() ?: $this->type;
     }
 
     /**
@@ -27,6 +72,6 @@ class UploadedFile extends \yii\web\UploadedFile implements UploadInterface
      */
     public function getSize()
     {
-        return $this->size;
+        return $this->saveManager->getSize() ?: $this->size;
     }
 }
