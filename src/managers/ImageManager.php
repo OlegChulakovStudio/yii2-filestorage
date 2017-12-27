ca39fdec2e33b3da30ea11740b0dae4fb131aa32
Ilya Karavaev <ik@chulakov.ru>
2017-12-27 19:02:06 +0300

    Доработка менеджера изображений

--- src/managers/ImageManager.php
+++ src/managers/ImageManager.php
@@ -9,53 +9,81 @@
 namespace chulakov\filestorage\managers;
 
 use yii\base\BaseObject;
+use chulakov\filestorage\ImageComponent;
 use chulakov\filestorage\uploaders\Event;
 use chulakov\filestorage\uploaders\ObserverInterface;
 use chulakov\filestorage\uploaders\UploadInterface;
+use yii\di\Instance;
 
 /**
  * Class SaveManager
  * @package chulakov\filestorage\managers
  */
-class FileManager extends BaseObject implements FileInterface, ListenerInterface
+class ImageManager extends BaseObject implements FileInterface, ListenerInterface
 {
     /**
-     * Подключение трейта для работы с изображениями
+     * Ширина
+     *
+     * @var integer
      */
-    use ImageTrait;
+    public $width;
+    /**
+     * Высота
+     *
+     * @var integer
+     */
+    public $height;
+    /**
+     * Кодировка картинки
+     *
+     * Возможные кодировки картинки
+     *
+     * jpg — return JPEG encoded image data
+     * png — return Portable Network Graphics (PNG) encoded image data
+     * gif — return Graphics Interchange Format (GIF) encoded image data
+     * tif — return Tagged Image File Format (TIFF) encoded image data
+     * bmp — return Bitmap (BMP) encoded image data
+     * ico — return ICO encoded image data
+     * psd — return Photoshop Document (PSD) encoded image data
+     * webp — return WebP encoded image data
+     * data-url — encode current image data in data URI scheme (RFC 2397)
+     *
+     * @var string
+     */
+    public $encode;
+    /**
+     * Путь к картинке с водяной меткой
+     *
+     * @var string
+     */
+    public $watermarkPath;
+    /**
+     * Позиционирование
+     *
+     * @var string
+     */
+    public $watermarkPosition = ImageComponent::POSITION_CENTER;
+    /**
+     * Качество изображения в процентах
+     *
+     * @var integer
+     */
+    public $quality = 100;
+    /**
+     * Название компонента для работы с изображениями
+     *
+     * @var string
+     */
+    public $imageClass;
+    /**
+     * @var ImageComponent
+     */
+    public $imageComponent;
 
     /**
      * @var UploadInterface
      */
     protected $uploader;
-
-    /**
-     * Событие на сохранение
-     *
-     * @param Event $event
-     */
-    public function handle(Event $event)
-    {
-        // Проверка корректного типа отправителя
-        if (!$this->validate($event->sender)) {
-            return;
-        }
-
-        // Обработка изображения
-        $this->processing();
-        $savedPath = $this->uploader->uploadPath($event->savedPath);
-        if ($this->saveImage($savedPath)) {
-            $event->needSave = false;
-        }
-        if ($event->needDelete && $this->deleteFile($event->filePath)) {
-            $event->needDelete = false;
-        }
-        //http://image.intervention.io/api/filesize
-        //Returns the size of the image file in bytes or false if image instance is not created from a file.
-        $this->uploader->setExtension($this->getExtension());
-        $this->uploader->setType($this->getType());
-        $this->uploader->setSize($this->getSize());
-    }
 
     /**
      * Присоединение к Observer
@@ -68,10 +96,37 @@ class FileManager extends BaseObject implements FileInterface, ListenerInterface
     }
 
     /**
+     * Событие на сохранение
+     *
+     * @param Event $event
+     * @throws \Exception
+     */
+    public function handle(Event $event)
+    {
+        // Проверка корректного типа отправителя
+        if (!$this->validate($event->sender)) {
+            return;
+        }
+        // Обработка изображения
+        $this->processing();
+        $newPath = $this->updatePath($event->savedPath);
+        if ($this->saveImage($newPath)) {
+            //http://image.intervention.io/api/filesize
+            //Returns the size of the image file in bytes or false if image instance is not created from a file.
+            $this->uploader->setName($newPath);
+            $this->uploader->setExtension($this->getExtension());
+            $this->uploader->setType($this->getType());
+            $this->uploader->setSize($this->getSize());
+            $event->needSave = false;
+        }
+    }
+
+    /**
      * Валидация файла для обработки
      *
      * @param object $uploader
      * @return bool
+     * @throws \Exception
      */
     protected function validate($uploader)
     {
@@ -89,11 +144,20 @@ class FileManager extends BaseObject implements FileInterface, ListenerInterface
 
     /**
      * Обработка файла
+     *
+     * @return bool
+     * @throws \Exception
      */
     protected function processing()
     {
-        $this->loadImage($this->uploader->getFile());
-        $this->transformation();
+        $manager = $this->getImageManager();
+        if ($manager->make($this->uploader->getFile())) {
+            $manager->resize($this->width, $this->height);
+            $manager->watermark($this->watermarkPath, $this->watermarkPosition);
+            $manager->convert($this->encode);
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -101,54 +165,37 @@ class FileManager extends BaseObject implements FileInterface, ListenerInterface
      *
      * @param string $savedPath
      * @return \Intervention\Image\Image
+     * @throws \Exception
      */
     protected function saveImage($savedPath)
     {
-        return $this->imageManager->getImage()->save($savedPath, $this->quality);
-    }
-
-    /**
-     * Удаление файла
-     *
-     * @param string $filePath
-     * @return bool
-     */
-    protected function deleteFile($filePath)
-    {
-        if (file_exists($filePath)) {
-            unlink($filePath);
-        }
-        return true;
+        return $this->getImageManager()->save($savedPath, $this->quality);
     }
 
     /**
      * Проверка файла на изображение
      *
      * @return bool
+     * @throws \Exception
      */
     public function isImage()
     {
-        if ($this->uploader) {
-            return strpos($this->getType(), 'image') !== false;
-        }
-        return false;
+        return strpos($this->getType(), 'image') !== false;
     }
 
     /**
      * Получить расширение файла
      *
      * @return mixed
+     * @throws \Exception
      */
     public function getExtension()
     {
         if (!empty($this->encode)) {
             return $this->encode;
         }
-        if ($this->isImage() &&
-            $ext = $this->imageManager->getImage()->extension) {
-            if (!empty($ext)) {
-                return $ext;
-            }
+        if ($ext = $this->getImageManager()->getExtension()) {
+            return $ext;
         }
         return $this->uploader->getExtension();
     }
@@ -157,11 +204,12 @@ class FileManager extends BaseObject implements FileInterface, ListenerInterface
      * Получение MIME типа файла
      *
      * @return string
+     * @throws \Exception
      */
     public function getType()
     {
-        if ($this->imageManager->hasImage()) {
-            return $this->imageManager->getImage()->mime;
+        if ($this->getImageManager()->hasImage()) {
+            return $this->getImageManager()->getMimeType();
         }
         return $this->uploader->getType();
     }
@@ -170,15 +218,46 @@ class FileManager extends BaseObject implements FileInterface, ListenerInterface
      * Получение размера файла
      *
      * @return integer
+     * @throws \Exception
      */
     public function getSize()
     {
-        if ($this->isImage() &&
-            $size = $this->imageManager->getImage()->filesize()) {
-            if (!empty($size)) {
-                return $size;
-            }
+        if ($size = $this->getImageManager()->getFileSize()) {
+            return $size;
         }
         return $this->uploader->getSize();
+    }
+
+    /**
+     * Обновление пути для сохранения
+     *
+     * @param string $originalPath
+     * @return string
+     * @throws \Exception
+     */
+    protected function updatePath($originalPath)
+    {
+        $path = dirname($originalPath);
+        $name = pathinfo($originalPath, PATHINFO_FILENAME) . '.' . $this->getExtension();
+        return implode(DIRECTORY_SEPARATOR, array_filter([$path, $name]));
+    }
+
+    /**
+     * Геттер для работы с imageComponent
+     * Получить менеджер работы с изображениями
+     *
+     * @return ImageComponent
+     * @throws \Exception
+     */
+    protected function getImageManager()
+    {
+        if (empty($this->imageComponent)) {
+            $this->imageComponent = $this->imageClass;
+            if (is_array($this->imageComponent) && empty($this->imageComponent['class'])) {
+                $this->imageComponent['class'] = $this->imageComponent;
+            }
+            $this->imageComponent = Instance::ensure($this->imageComponent);
+        }
+        return $this->imageComponent;
     }
 }
