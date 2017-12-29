3a5a476812202a80c8bb0cff44a17bacc882c9e0
Vladimir <tzendos@mail.ru>
2017-12-29 17:21:21 +0300

    Тестирование системы Observer

---
+++ tests/ObserverTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Файл класса ObserverTest
+ *
+ * @copyright Copyright (c) 2017, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+namespace chulakov\filestorage\tests;
+
+use chulakov\filestorage\observer\Event;
+use chulakov\filestorage\uploaders\UploadedFile;
+
+/**
+ * Class ObserverTest
+ * @package chulakov\filestorage\tests
+ */
+class ObserverTest extends TestCase
+{
+    /**
+     * Подключение трейта с генерированием uploader и event
+     */
+    use UploaderMockTrait;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockApplication();
+        $this->generateFakeUploader();
+    }
+
+    /**
+     * Тумблер для проверки срабатывания события
+     *
+     * @var bool
+     */
+    public $tumbler = false;
+
+    /**
+     * Handle для регестрирования события
+     */
+    public function handle()
+    {
+        // переключить тумблер, подтверждая этим срабатывание события
+        $this->tumbler = true;
+    }
+
+    /**
+     * Проверка работоспомобности Observer его функции trigger
+     */
+    public function testTrigger()
+    {
+        // установка начального положения тумблера
+        $this->tumbler = false;
+
+        /** @var UploadedFile $uploader */
+        $uploader = self::$fileUploader;
+        // регистрирование события
+        $uploader->events[Event::SAVE_EVENT][] = [$this, 'handle'];
+        /** @var Event $event */
+        $event = $this->createEvent();
+        // срабатывание слушателей
+        $uploader->trigger(Event::SAVE_EVENT, $event);
+
+        $this->assertTrue($this->tumbler);
+    }
+}
