bd098b5ccd884bd821e73ab32232b78f94a8eff0
Vladimir <tzendos@mail.ru>
2017-12-29 17:20:07 +0300

    Тестовая модель изображения

---
+++ tests/models/ImageModelTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Файл класса Image
+ *
+ * @copyright Copyright (c) 2017, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+namespace chulakov\filestorage\tests;
+
+/**
+ * Class Image
+ * @package chulakov\filestorage\tests
+ */
+class ImageModelTest extends FileModelTest
+{
+}
