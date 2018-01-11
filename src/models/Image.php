02803b7a22fc412c68abc1913d7b3e3d47f1ba0c
Vladimir <tzendos@mail.ru>
2018-01-11 18:17:36 +0300

    Комментирование прокинутых функций

--- src/models/Image.php
+++ src/models/Image.php
@@ -10,6 +10,7 @@ namespace chulakov\filestorage\models;
 
 use yii\helpers\ArrayHelper;
 use chulakov\filestorage\params\ThumbParams;
+use chulakov\filestorage\params\ImageMakeParams;
 use chulakov\filestorage\behaviors\ImageBehavior;
 
 /**
@@ -18,6 +19,11 @@ use chulakov\filestorage\behaviors\ImageBehavior;
  *
  * @method string thumb(ThumbParams $thumbParams)
  * @method bool removeAllThumbs()
+ *
+ * @method bool contain(ImageMakeParams $params)   Вписывание изображения в область путем пропорционального масштабирования без обрезки
+ * @method bool cover(ImageMakeParams $params)     Заполнение обаласти частью изображения с обрезкой исходного, отталкиваясь от точки позиционировани
+ * @method bool widen(ImageMakeParams $params)     Масштабирование по ширине без обрезки краев
+ * @method bool heighten(ImageMakeParams $params)  Масштабирование по высоте без обрезки краев
  */
 class Image extends BaseFile
 {
