e7791dd3cd105dd13806474361bbc03551ea5e12
Vladimir <tzendos@mail.ru>
2018-01-12 12:53:06 +0300

    Комментирование прокидуемых функций

--- src/models/Image.php
+++ src/models/Image.php
@@ -9,21 +9,19 @@
 namespace chulakov\filestorage\models;
 
 use yii\helpers\ArrayHelper;
-use chulakov\filestorage\params\ThumbParams;
-use chulakov\filestorage\params\ImageMakeParams;
 use chulakov\filestorage\behaviors\ImageBehavior;
 
 /**
  * Class Image
  * @package chulakov\filestorage\models
  *
- * @method string thumb(ThumbParams $thumbParams)
+ * @method string thumb($w = 0, $h = 0, $q = 0, $p = null)
  * @method bool removeAllThumbs()
  *
- * @method bool contain(ImageMakeParams $params)   Вписывание изображения в область путем пропорционального масштабирования без обрезки
- * @method bool cover(ImageMakeParams $params)     Заполнение обаласти частью изображения с обрезкой исходного, отталкиваясь от точки позиционировани
- * @method bool widen(ImageMakeParams $params)     Масштабирование по ширине без обрезки краев
- * @method bool heighten(ImageMakeParams $params)  Масштабирование по высоте без обрезки краев
+ * @method bool contain($width, $height, $quality)          Вписывание изображения в область путем пропорционального масштабирования без обрезки
+ * @method bool cover($width, $height, $quality, $position) Заполнение обаласти частью изображения с обрезкой исходного, отталкиваясь от точки позиционировани
+ * @method bool widen($width, $quality)                     Масштабирование по ширине без обрезки краев
+ * @method bool heighten($height, $quality)                 Масштабирование по высоте без обрезки краев
  */
 class Image extends BaseFile
 {
