a6be07c935f843c373938534657be7724bcf0e4a
Vladimir <tzendos@mail.ru>
2018-01-22 19:54:24 +0300

    Тестирование ImageManager

--- tests/ImageManagerTest.php
+++ tests/ImageManagerTest.php
@@ -8,6 +8,7 @@
 
 namespace chulakov\filestorage\tests;
 
+use chulakov\filestorage\image\Position;
 use chulakov\filestorage\ImageComponent;
 use chulakov\filestorage\managers\ImageManager;
 use chulakov\filestorage\uploaders\UploadedFile;
@@ -52,7 +53,7 @@ class ImageManagerTest extends TestCase
                     'encode' => 'jpg',
                     'quality' => 100, // в процентах
                     'watermarkPath' => \Yii::getAlias('@tests/data') . '/images/watermark/watermark.png',
-                    'watermarkPosition' => ImageComponent::POSITION_CENTER,
+                    'watermarkPosition' => Position::CENTER,
                     'imageClass' => ImageComponent::className()
                 ]
             ]
@@ -66,34 +67,22 @@ class ImageManagerTest extends TestCase
      */
     public function testImageManage()
     {
-        // путь к сохраняемому файлу
-        $path = \Yii::getAlias('@tests/runtime') . '/image.jpg';
+        // Путь к сохраняемому файлу
+        $path = \Yii::getAlias('@tests/runtime') . '/images/image.jpg';
         /** @var UploadedFile $uploader */
         $uploader = UploadedFile::getInstance(new ImageModelTest(), 'imageManager');
-
-        // конфигурирование слушателей
+        // Конфигурирование слушателей
         $uploader->configure($this->getListener());
-
-        // установить системное имя
+        // Установить системное имя
         $uploader->setSysName('image');
-
-        // сохранение файла
+        // Сохранение файла
         $uploader->saveAs($path, false);
-
-        if (!file_exists($path)) {
-            $this->throwException(new \Exception('Файл не был загружен.'));
-        }
-
-        // Обновление path на который в результате должен так измениться
-        // images - группа ImageManager
-        $path = \Yii::getAlias('@tests/runtime') . '/images/image.jpg';
 
         $this->assertInstanceOf(UploadedFile::className(), $uploader);
         $this->assertEquals(filesize($path), $uploader->getSize());
         $this->assertEquals(basename($path), $uploader->getName());
         $this->assertEquals(mime_content_type($path), $uploader->getType());
-
-        // удаление сгенерированного файла
+        // Удаление сгенерированного файла
         unlink($path);
     }
 }
