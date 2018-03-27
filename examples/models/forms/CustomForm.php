ed01a028e14df66d78c1550f59f8ec865491dfbe
Vladimir <tzendos@mail.ru>
2018-03-27 15:48:26 +0300

    Чистка форм

---
+++ examples/models/forms/CustomForm.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Файл класса CustomForm
+ *
+ *
+ * @copyright Copyright (c) 2018, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+use yii\base\Model;
+use backend\managers\MailManager;
+use chulakov\filestorage\models\BaseFile;
+use chulakov\filestorage\uploaders\UploadedFile;
+use chulakov\filestorage\behaviors\FileUploadBehavior;
+
+/**
+ * Class CustomForm
+ * @package backend\models
+ *
+ * @method BaseFile[] upload()
+ */
+class CustomForm extends Model
+{
+    /**
+     * Загружаемый файл
+     *
+     * @var array
+     */
+    public $file;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['file'], 'required']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => FileUploadBehavior::className(),
+                'attribute' => 'file',
+                'group' => 'files',
+                'fileStorage' => 'fileStorage',
+                'repository' => UploadedFile::class,
+                'repositoryOptions' => [
+                    'listeners' =>
+                        [
+                            [
+                                'class' => MailManager::class,
+                            ]
+                        ],
+                ]
+            ],
+        ];
+    }
+}
---
+++ examples/models/forms/RemoteFileForm.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Файл класса RemoteFileForm
+ *
+ * @copyright Copyright (c) 2018, Oleg Chulakov Studio
+ * @link http://chulakov.com/
+ */
+
+use yii\base\Model;
+use chulakov\filestorage\image\Position;
+use chulakov\filestorage\models\BaseFile;
+use chulakov\filestorage\managers\ImageManager;
+use chulakov\filestorage\managers\ThumbsManager;
+use chulakov\filestorage\uploaders\RemoteUploadedFile;
+use chulakov\filestorage\behaviors\FileUploadBehavior;
+
+/**
+ * Class RemoteFileForm
+ * @package backend\models
+ *
+ * @method BaseFile[] upload()
+ */
+class RemoteFileForm extends Model
+{
+    /**
+     * Ссылка на файл
+     *
+     * @var array
+     */
+    public $link;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['link'], 'required']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => FileUploadBehavior::className(),
+                'attribute' => 'link',
+                'group' => 'files',
+                'fileStorage' => 'fileStorage',
+                'repository' => RemoteUploadedFile::class,
+                'repositoryOptions' => [
+                    'listeners' =>
+                        [
+                            [
+                                'class' => ThumbsManager::className(),
+                                'encode' => 'jpg',
+                                'quality' => 80,
+                                'watermarkPath' => '/path/to/watermark/watermark.png',
+                                'watermarkPosition' => Position::CENTER,
+                                'imageComponent' => 'imageComponent',
+                            ],
+                            [
+                                'class' => ImageManager::className(),
+                                'width' => 640,
+                                'height' => 480,
+                                'encode' => 'jpg',
+                                'quality' => 100,
+                                'watermarkPath' => '/path/to/watermark/watermark.png',
+                                'watermarkPosition' => Position::CENTER,
+                                'imageComponent' => 'imageComponent',
+                                'accessRole' => 'role_example',
+                            ]
+                        ],
+                ]
+            ],
+        ];
+    }
+}
