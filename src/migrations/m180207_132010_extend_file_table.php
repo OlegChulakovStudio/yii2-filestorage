bb02795cb55e98260535ffd9a0f9659beefbbf3a
Aleksei Rodionov <rodial.pro@gmail.com>
2019-06-20 12:03:20 +0400

    Fix migration for use postgresql

--- src/migrations/m180207_132010_extend_file_table.php
+++ src/migrations/m180207_132010_extend_file_table.php
@@ -10,7 +10,15 @@ class m180207_132010_extend_file_table extends Migration
     public function up()
     {
         $this->addColumn('{{%file}}', 'object_type', $this->string(16)->after('object_id')->comment('Дополнительная идентификация типа файла'));
-        $this->alterColumn('{{%file}}', 'object_id', $this->integer()->null()->comment('Уникальный идетификатор сущности для доп. группировки файлов'));
+
+        if ($this->db->getDriverName() == 'pgsql') {
+            $this->execute('ALTER TABLE {{%file}} ALTER COLUMN "object_id" DROP DEFAULT');
+            $this->alterColumn('{{%file}}', 'object_id', 'integer USING object_id::integer');
+            $this->execute('ALTER TABLE {{%file}} ALTER COLUMN "object_id" SET DEFAULT NULL');
+            $this->addCommentOnColumn('{{%file}}', 'object_id', 'Уникальный идетификатор сущности для доп. группировки файлов');
+        } else {
+            $this->alterColumn('{{%file}}', 'object_id', $this->integer()->null()->comment('Уникальный идетификатор сущности для доп. группировки файлов'));
+        }
     }
 
     /**
