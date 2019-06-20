b9aca1d7ea3ebf9b8d396a74213256dba6f757f3
Aleksei Rodionov <rodial.pro@gmail.com>
2019-06-20 17:21:19 +0400

    Fix migration for use postgresql, fix down method

--- src/migrations/m180207_132010_extend_file_table.php
+++ src/migrations/m180207_132010_extend_file_table.php
@@ -10,14 +10,13 @@ class m180207_132010_extend_file_table extends Migration
     public function up()
     {
         $this->addColumn('{{%file}}', 'object_type', $this->string(16)->after('object_id')->comment('Дополнительная идентификация типа файла'));
-
-        if ($this->db->getDriverName() == 'pgsql') {
-            $this->execute('ALTER TABLE {{%file}} ALTER COLUMN "object_id" DROP DEFAULT');
+        if ($this->db->driverName == 'pgsql') {
+            $this->alterColumn('{{%file}}', 'object_id', 'DROP DEFAULT');
             $this->alterColumn('{{%file}}', 'object_id', 'integer USING object_id::integer');
-            $this->execute('ALTER TABLE {{%file}} ALTER COLUMN "object_id" SET DEFAULT NULL');
+            $this->alterColumn('{{%file}}', 'object_id', 'SET DEFAULT NULL');
             $this->addCommentOnColumn('{{%file}}', 'object_id', 'Уникальный идетификатор сущности для доп. группировки файлов');
         } else {
-            $this->alterColumn('{{%file}}', 'object_id', $this->integer()->null()->comment('Уникальный идетификатор сущности для доп. группировки файлов'));
+            $this->alterColumn('{{%file}}', 'object_id', $this->integer()->comment('Уникальный идетификатор сущности для доп. группировки файлов'));
         }
     }
 
@@ -27,6 +26,13 @@ class m180207_132010_extend_file_table extends Migration
     public function down()
     {
         $this->dropColumn('{{%file}}', 'object_type');
-        $this->alterColumn('{{%file}}', 'object_id', $this->string(11)->null()->comment('Уникальный идетификатор сущности для доп. группировки файлов. Например, у новости с object_id=5 может быть 10 изображений'));
+        if ($this->db->driverName == 'pgsql') {
+            $this->alterColumn('{{%file}}', 'object_id', 'DROP DEFAULT');
+            $this->alterColumn('{{%file}}', 'object_id', 'varchar(11) USING object_id::varchar(11)');
+            $this->alterColumn('{{%file}}', 'object_id', 'SET DEFAULT NULL');
+            $this->addCommentOnColumn('{{%file}}', 'object_id', 'Уникальный идетификатор сущности для доп. группировки файлов. Например, у новости с object_id=5 может быть 10 изображений');
+        } else {
+            $this->alterColumn('{{%file}}', 'object_id', $this->string(11)->null()->comment('Уникальный идетификатор сущности для доп. группировки файлов. Например, у новости с object_id=5 может быть 10 изображений'));
+        }
     }
 }
